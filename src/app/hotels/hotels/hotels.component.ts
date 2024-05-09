422d867c7fa301af3ab9ec710b45962c7fe1f8ed
otari nozadze <otikonozadze@gmail.com>
2024-05-09 21:10:35 +0400

    added scroll bar on hotels

--- src/app/hotels/hotels/hotels.component.ts
+++ src/app/hotels/hotels/hotels.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiCallsService } from '../../shared/api/api-calls.service';
+import { SpinerService } from '../../shared/spiner/spiner.service';
 
 @Component({
   selector: 'app-hotels',
@@ -7,16 +8,19 @@ import { ApiCallsService } from '../../shared/api/api-calls.service';
   styleUrl: './hotels.component.scss',
 })
 export class HotelsComponent implements OnInit {
-  constructor(private api: ApiCallsService) {}
+  constructor(private api: ApiCallsService, private spiner: SpinerService) {}
   allHotels: any;
   ngOnInit(): void {
+    this.spiner.loadingScrollBar();
     if (!this.api.allHotels) {
       this.api.gettingHotels().subscribe((data: any) => {
         this.api.allHotels = data;
         this.allHotels = this.api.allHotels;
+        this.spiner.finishedLoadingScrollBar();
       });
     } else {
       this.allHotels = this.api.allHotels;
+      this.spiner.finishedLoadingScrollBar();
     }
   }
 }
